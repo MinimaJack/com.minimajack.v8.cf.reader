2b2b1a7ee43255ea700bbfe17af37f7658c27f49
Evgeniy <minimajack@gmail.com>
2017-04-20 16:25:39 +0300

    Compare modify date for virtual & real file before save to disk. If them the same, write operation skipped.

--- src/main/java/com/minimajack/v8/model/FileReader.java
+++ src/main/java/com/minimajack/v8/model/FileReader.java
@@ -6,6 +6,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
 import java.util.concurrent.RecursiveTask;
 
 import com.google.common.io.ByteStreams;
@@ -26,37 +30,52 @@ public class FileReader
     protected Boolean compute()
     {
         Boolean allOk = true;
-        try (InputStream dataStream = file.getBody().getDataStream();
-            OutputStream fos = new BufferedOutputStream( new FileOutputStream( getOrCreateFile( file ) ) ))
+        File fileReal = null;
+        try
         {
-            ByteStreams.copy( dataStream, fos );
+            fileReal = getOrCreateFile( file );
+            Path p = fileReal.toPath();
+            long lastModifyVirtual = file.getAttributes().getModifyDate().getTime();
+            long createdVirtual = file.getAttributes().safeGetCreationDate().getTime();
+
+            FileTime attrs = Files.getLastModifiedTime( p, LinkOption.NOFOLLOW_LINKS );
+            if ( attrs.toMillis() == lastModifyVirtual )
+            {
+                return allOk;
+            }
+            try (InputStream dataStream = file.getBody().getDataStream();
+                OutputStream fos = new BufferedOutputStream( new FileOutputStream( fileReal ) ))
+            {
+                ByteStreams.copy( dataStream, fos );
+                Files.setAttribute( p, "creationTime", FileTime.fromMillis( createdVirtual ) );
+                Files.setAttribute( p, "lastModifiedTime", FileTime.fromMillis( lastModifyVirtual ) );
+            }
+            catch ( Exception e )
+            {
+                System.out.println( "creation time " + createdVirtual + "\t modtime: " + lastModifyVirtual );
+                allOk = false;
+            }
         }
-        catch ( Exception e )
+        catch ( IOException e )
         {
             e.printStackTrace();
             allOk = false;
         }
+
         return allOk;
     }
 
     private final File getOrCreateFile( V8File v8File )
         throws IOException
     {
-        String name = v8File.getAttributes().getName();
+        String name = v8File.getAttributes().getName().trim();
         String path = v8File.getContext().getPath();
         File file = new File( path );
         file.mkdirs();
-        file = new File( path + "/" + name.trim() + ".txt" );
+        file = new File( path + File.separator + name + ".txt" );
         if ( !file.exists() )
         {
-            try
-            {
-                file.createNewFile();
-            }
-            catch ( Exception e )
-            {
-                e.printStackTrace();
-            }
+            file.createNewFile();
         }
         return file;
     }
