c42c21393cbc0b3ce7891ac05673a5bdb70066f5
Evgeniy <minimajack@gmail.com>
2017-05-15 15:30:54 +0300

    Add metadata for DefinedTypes
    Now can parse all erp DefinedTypes

---
+++ src/main/java/com/minimajack/v8/metadata/definedtypes/DefinedTypesMetaData.java
@@ -0,0 +1,15 @@
+package com.minimajack.v8.metadata.definedtypes;
+
+import com.minimajack.v8.annotation.V8Class;
+import com.minimajack.v8.metadata.innerclass.V8InnerClass;
+
+import java.util.List;
+
+@V8Class
+public class DefinedTypesMetaData {
+
+  public Integer version;
+
+  public DefinedTypesBlock info;
+  public List<V8InnerClass> sections;
+}
