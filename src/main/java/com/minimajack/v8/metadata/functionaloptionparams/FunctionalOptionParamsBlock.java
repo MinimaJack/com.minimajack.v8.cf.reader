2f740559d3541813639fc13d19989a8d2e3b45b4
Evgeniy <minimajack@gmail.com>
2017-05-16 09:10:47 +0300

    Add metadata for FunctionalOptionParams
    Now can parse all upp & erp functional option params.

---
+++ src/main/java/com/minimajack/v8/metadata/functionaloptionparams/FunctionalOptionParamsBlock.java
@@ -0,0 +1,16 @@
+package com.minimajack.v8.metadata.functionaloptionparams;
+
+import com.minimajack.v8.annotation.V8Class;
+import com.minimajack.v8.annotation.V8Version;
+import com.minimajack.v8.metadata.external.common.V8MetaShortName;
+import com.minimajack.v8.metadata.external.unknown.UnkObjectListTypes;
+
+@V8Class
+public class FunctionalOptionParamsBlock {
+
+  @V8Version
+  public Integer version;
+  public V8MetaShortName v8mn;
+  public UnkObjectListTypes unk5;
+
+}
---
+++ src/main/java/com/minimajack/v8/metadata/functionaloptionparams/FunctionalOptionParamsDescription.java
@@ -0,0 +1,14 @@
+package com.minimajack.v8.metadata.functionaloptionparams;
+
+import com.minimajack.v8.annotation.V8Class;
+import com.minimajack.v8.metadata.inner.classes.V8InnerClass;
+
+import java.util.List;
+
+@V8Class
+public class FunctionalOptionParamsDescription {
+
+  public Integer version;
+  public FunctionalOptionParamsBlock fopb;
+  public List<V8InnerClass> sections;
+}
