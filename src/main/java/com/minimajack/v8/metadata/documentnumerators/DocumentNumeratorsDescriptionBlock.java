2a93ae3fcbdf307aca1c9250ad498df2cb8a8ada
Evgeniy <minimajack@gmail.com>
2017-05-17 08:04:20 +0300

    Add DocumentNumerators

---
+++ src/main/java/com/minimajack/v8/metadata/documentnumerators/DocumentNumeratorsDescriptionBlock.java
@@ -0,0 +1,18 @@
+package com.minimajack.v8.metadata.documentnumerators;
+
+import com.minimajack.v8.annotation.V8Class;
+import com.minimajack.v8.annotation.V8Version;
+import com.minimajack.v8.metadata.external.common.V8MetaShortName;
+
+@V8Class
+public class DocumentNumeratorsDescriptionBlock {
+
+  @V8Version
+  public Integer version;
+  public V8MetaShortName v8mn;
+  public Integer unk1;
+  public Integer unk2;
+  public Integer unk3;
+  public Integer unk4;
+  public Integer unk5;
+}
