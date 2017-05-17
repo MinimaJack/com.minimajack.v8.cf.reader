b87a483c6e538b356fe127264e09aff348ee0698
Evgeniy <minimajack@gmail.com>
2017-05-17 15:09:07 +0300

    Add CommonTemplates

---
+++ src/main/java/com/minimajack/v8/metadata/commontemplates/CommonTemplatesDescription.java
@@ -0,0 +1,17 @@
+package com.minimajack.v8.metadata.commontemplates;
+
+import com.minimajack.v8.annotation.V8Class;
+import com.minimajack.v8.metadata.inner.classes.V8InnerClass;
+
+import java.util.List;
+
+@V8Class
+public class CommonTemplatesDescription {
+
+  public Integer version;
+
+  public CommonTemplatesDescriptionInner formInnerDescription;
+
+  public List<V8InnerClass> sections;
+
+}
---
+++ src/main/java/com/minimajack/v8/metadata/commontemplates/CommonTemplatesDescriptionInner.java
@@ -0,0 +1,13 @@
+package com.minimajack.v8.metadata.commontemplates;
+
+import com.minimajack.v8.annotation.V8Class;
+import com.minimajack.v8.metadata.external.common.V8MetaShortName;
+
+@V8Class
+public class CommonTemplatesDescriptionInner {
+
+  public Integer version;
+  public V8MetaShortName md;
+  public Integer unk1;
+
+}
