61fb2943a63d838028051ec0a483b792c6983cd1
Evgeniy <minimajack@gmail.com>
2017-05-17 15:05:36 +0300

    Add CommonPictures

---
+++ src/main/java/com/minimajack/v8/metadata/commonpictures/CommonPicturesDescriptionInner.java
@@ -0,0 +1,12 @@
+package com.minimajack.v8.metadata.commonpictures;
+
+import com.minimajack.v8.annotation.V8Class;
+import com.minimajack.v8.metadata.external.common.V8MetaShortName;
+
+@V8Class
+public class CommonPicturesDescriptionInner {
+
+  public Integer version;
+  public V8MetaShortName md;
+
+}
