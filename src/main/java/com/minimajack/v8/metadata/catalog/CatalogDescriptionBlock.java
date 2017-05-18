f3ff56cf151dc6ab0b9b38714595da10ac692f12
Evgeniy <minimajack@gmail.com>
2017-05-18 16:57:48 +0300

    typo

--- src/main/java/com/minimajack/v8/metadata/catalog/CatalogDescriptionBlock.java
+++ src/main/java/com/minimajack/v8/metadata/catalog/CatalogDescriptionBlock.java
@@ -32,7 +32,7 @@ public class CatalogDescriptionBlock {
   public Integer version;
   public UUID unk1;
   public UUID unk2;
-  public UUID unk3;
+  public UUID uuidInRegisters;
   public UUID unk4;
   public UUID unk5;
   public UUID unk6;
